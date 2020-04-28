eb2a54c05f273538b2629e39fdf307065954bc22
Edel-M <ser.nei.2819@gmail.com>
2020-04-28 13:30:08 -0600

    completando navegacion parcial

---
+++ EXA_U2/app/src/main/java/com/example/examen_u2/MostrarActivity.java
@@ -0,0 +1,14 @@
+package com.example.examen_u2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MostrarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lista_restaurantes);
+    }
+}
