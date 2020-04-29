51acb4914b79144ac1cbc756bbf7e7b8617131bf
RosiqueVera <rosiqueVera@gmail.com>
2020-04-29 05:35:20 -0600

    Corrigiendo los datos de Restaurante y terminando el adaptador

--- EXA_U2/app/src/main/java/com/example/examen_u2/Restaurante.java
+++ EXA_U2/app/src/main/java/com/example/examen_u2/Restaurante.java
@@ -5,11 +5,15 @@ public class Restaurante {
     private String nombre;
     private String descripcion;
     private String direccion_tel;
+    private int img;
+    private float calif;
 
-    public Restaurante(String nombre, String descripcion, String direccion_tel) {
+    public Restaurante(String nombre, String descripcion, String direccion_tel, int img, float calif) {
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.direccion_tel = direccion_tel;
+        this.img = img;
+        this.calif = calif;
     }
 
     public String getNombre() {
@@ -24,4 +28,7 @@ public class Restaurante {
         return direccion_tel;
     }
 
+    public int getImg() { return img; }
+
+    public float getCalif() { return calif; }
 }
--- EXA_U2/app/src/main/java/com/example/examen_u2/RestauranteAdapter.java
+++ EXA_U2/app/src/main/java/com/example/examen_u2/RestauranteAdapter.java
@@ -1,9 +1,13 @@
 package com.example.examen_u2;
 
+import android.app.Activity;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -28,6 +32,29 @@ public class RestauranteAdapter extends ArrayAdapter<Restaurante> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        //ConvertView representa mi layout creado
+        if (convertView ==null){ //Primera vez que se ejecuta el layout
+            convertView = ((Activity) context).getLayoutInflater().inflate(resource, parent,false);
+        }
+        //Hay que recuperar los componentes
+        TextView txtNombre, txtDesc, txtAddress;
+        ImageView imgRestaurante;
+        RatingBar califRestaurante;
+
+        txtNombre = convertView.findViewById(R.id.Name_Restaurante);
+        txtDesc = convertView.findViewById(R.id.desc_Restaurante);
+        txtAddress = convertView.findViewById(R.id.address_Restaurante);
+
+        imgRestaurante = convertView.findViewById(R.id.img_Restaurante);
+
+        califRestaurante = convertView.findViewById(R.id.calif_Restaurante);
+
+
+        txtNombre.setText(getItem(position).getNombre());
+        txtDesc.setText(getItem(position).getDescripcion());
+        txtAddress.setText(getItem(position).getDireccion_tel());
+        imgRestaurante.setImageResource(getItem(position).getImg());
+        califRestaurante.setRating(getItem(position).getCalif());
+        return convertView;
     }
 }
