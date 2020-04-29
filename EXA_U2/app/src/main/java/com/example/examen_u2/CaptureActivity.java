60f8f14e5d46fb5298edcb41510706c751d70e19
RosiqueVera <rosiqueVera@gmail.com>
2020-04-29 06:16:14 -0600

    Configurando el botón de guardar en CaptureActivity

--- EXA_U2/app/src/main/java/com/example/examen_u2/CaptureActivity.java
+++ EXA_U2/app/src/main/java/com/example/examen_u2/CaptureActivity.java
@@ -7,27 +7,38 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
 
 public class CaptureActivity extends AppCompatActivity {
 public List<Restaurante> restaurantes;
-int img_id;
+
 EditText txt_Name;
 EditText txt_Desc;
 EditText txt_Direc_Num;
 Button btn_Save;
+ImageView imgRest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_capture);
+
         txt_Name = findViewById(R.id.txt_Name);
         txt_Desc = findViewById(R.id.txt_Desc);
         txt_Direc_Num = findViewById(R.id.txt_Address);
         btn_Save = findViewById(R.id.save);
+        imgRest = findViewById(R.id.imgRest);
+        Platillo  platillo = new Platillo()
+        Restaurante restaurante = new Restaurante(txt_Name.getText(),txt_Desc.getText(),txt_Direc_Num.getText(),imgRest.getDrawable().)
 
-
+        btn_Save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                restaurantes.add()
+            }
+        });
     }
                                                                                                     /*Este método se activa cada vez que se presiona con el botón back del dispositivo
                                                                                                     * y al presionarlo vuelve a generar un intent para ir a la clase Main además
