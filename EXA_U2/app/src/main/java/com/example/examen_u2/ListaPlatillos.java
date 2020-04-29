74edf03ec20edfd95bd3a8a24ae5e94c6ae99e4c
Edel-M <ser.nei.2819@gmail.com>
2020-04-29 13:50:08 -0600

    Mostrando imagenes con adapter

--- EXA_U2/app/src/main/java/com/example/examen_u2/ListaPlatillos.java
+++ EXA_U2/app/src/main/java/com/example/examen_u2/ListaPlatillos.java
@@ -8,16 +8,23 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ListaPlatillos extends AppCompatActivity {
 
     ListView listPlatillo;
+    Platillo[] platillos = {
+            new Platillo(R.drawable.enchilada),
+            new Platillo(R.drawable.mole),
+            new Platillo(R.drawable.ensalada),
+            new Platillo(R.drawable.comidarapida),
+            new Platillo(R.drawable.tlacoyo)};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.lista_imagenes);
+        setContentView(R.layout.lista_platillos);
         listPlatillo = findViewById(R.id.listPlatillo);
+listPlatillo.setAdapter(new PlatilloAdapter(this, R.layout.item_platillo, platillos));
+
 
 
     }
 
-    Platillo[] platillos = {new Platillo(R.drawable.enchilada), new Platillo(R.drawable.mole)
-            , new Platillo(R.drawable.ensalada), new Platillo(R.drawable.comidarapida), new Platillo(R.drawable.tlacoyo)};
+
 }
--- EXA_U2/app/src/main/java/com/example/examen_u2/MostrarActivity.java
+++ EXA_U2/app/src/main/java/com/example/examen_u2/MostrarActivity.java
@@ -10,5 +10,6 @@ public class MostrarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lista_restaurantes);
+
     }
 }
