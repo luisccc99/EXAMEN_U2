0af5d08e36fd9071adf86b171ac7c6d5ac9c71bd
Edel-M <ser.nei.2819@gmail.com>
2020-04-29 18:18:47 -0600

    Añadiendo evaluacion

--- EXA_U2/app/src/main/java/com/example/examen_u2/MostrarActivity.java
+++ EXA_U2/app/src/main/java/com/example/examen_u2/MostrarActivity.java
@@ -2,7 +2,11 @@ package com.example.examen_u2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -12,6 +16,7 @@ import java.util.List;
 
 public class MostrarActivity extends AppCompatActivity {
     ListView listRestautante;
+
     Restaurante[] restaurantesPorDefault = {
             new Restaurante("Mercado Reforma", "Breakfast Outdoor seating", "Periferico de la Juventud 5700", R.drawable.platillo, 3),
             new Restaurante("Mercado Reforma", "Breakfast Outdoor seating", "Periferico de la Juventud 5700", R.drawable.platillo, 3),
@@ -27,12 +32,21 @@ public class MostrarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lista_restaurantes);
-        List<Restaurante> restaurantes = new ArrayList<Restaurante>(Arrays.asList(restaurantesPorDefault));
+        final List<Restaurante> restaurantes = new ArrayList<Restaurante>(Arrays.asList(restaurantesPorDefault));
 
         ArrayAdapter<Restaurante> arrayAdapter = new RestauranteAdapter(MostrarActivity.this, R.layout.item_restaurante, (ArrayList<Restaurante>) restaurantes);
 
 arrayAdapter.notifyDataSetChanged();
         listRestautante = findViewById(R.id.listRest);
         listRestautante.setAdapter(arrayAdapter);
+        listRestautante.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent restauranteSelec = new Intent();
+                restauranteSelec.putExtra("Restaurante Seleccionado", restaurantes.getPosition);
+                setResult(Activity.RESULT_OK,restauranteSelec);
+                finish();
+            }
+        });
     }
 }
